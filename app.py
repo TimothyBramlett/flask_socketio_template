4c1f07ed1074e84baa8849a704a0b1b377d57094
Timothy Bramlett <timothy.bramlett@gmail.com>
2017-02-15 09:07:44 -0600

    Adding latest code

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, send
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
 import eventlet
+import logging
+import platform
+import os
 
+thread = None
+
+if platform.platform().startswith('Windows'):
+    logging_file = os.path.dirname(os.path.realpath(__file__)) + r'\test.log'
+
+logging.basicConfig(filename=logging_file, level=logging.DEBUG)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while count < 100:
+        socketio.sleep(1)
+        count += 1
+        print(count)
+        socketio.emit('number_counter_msg',
+                      count,
+                      namespace='/counter')
 
 
 @app.route('/', methods=['GET', 'POST']) # The acceptable HTTP methods for this
@@ -13,18 +33,40 @@ def index():
     return render_template('index.html')
 
 
-@socketio.on('client_connected')
-def test_message(message):
-    #emit('my response', {'data': message['data']})
-    print('my response', {'data': message['data']})
 
-    upper_limit = 20
-    x = 0
-    while x < upper_limit:
-        print(x)
-        emit('number_counter_msg', x)
-        eventlet.sleep(1)
-        x += 1
+
+@socketio.on('client_connected', namespace='/counter')
+def start_counter(message):
+
+    thread = socketio.start_background_task(target=background_thread)
+    emit('number_counter_msg', 'Thread Started...')
+
+
+
+# # old version
+# @socketio.on('client_connected', namespace='/counter')
+# def start_counter(message):
+#     global thread
+#     if thread is None:
+#         thread = socketio.start_background_task(target=background_thread)
+#     else:
+#         print('Thread already started.')
+#     emit('number_counter_msg', 'Thread Started...')
+
+
+
+# @socketio.on('client_connected', namespace='/counter')
+# def test_message(message):
+#     #emit('my response', {'data': message['data']})
+#     print('my response', {'data': message['data']})
+#
+#     upper_limit = 10
+#     x = 0
+#     while x < upper_limit:
+#         print('{} - {}'.format(x, request.sid))
+#         emit('number_counter_msg', x, namespace='/counter')
+#         eventlet.greenthread.sleep(2)
+#         x += 1
 
 
 # @socketio.on('my broadcast event', namespace='/test')
